e58412a468f8a30e9c193e4c84ad338b32273569
isaac233@naver.com <isaac233@naver.com>
2023-01-01 19:18:59 +0900

    2023-01-01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import Variable from "./R010_VAriable";
+import ArrowFunc from "./R013_ArrowFunction";
 /*
+import ClassPrototype from "./R012_Class&Prototype";
+import SpreadOperator from "./R011_SpreadOperator";
+import Variable from "./R010_VAriable";
 import ImportConponent from "./R003_ImportConpoenet";
 import LifecycleEx from "./R004_LifecycleEx";
 import Es6 from "./R009_Es6";
@@ -13,7 +16,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>css 적용하기</p>
-      <Variable />
+      <ArrowFunc />
     </div>
   );
 }
